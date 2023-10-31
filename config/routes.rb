bceca0a1871db6318f7f2864f500c96cb5598fac
aydinkazim <kzmaydn@hotmail.com>
2023-10-31 13:45:13 +0300

    rails g scaffold post title body:text

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :pages, only: [:index]
   get 'what/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
